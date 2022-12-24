53e846b6da63501dec9ffb47f05cf7e78b1d3eb0
tumuyan <tumuyan@gmail.com>
2022-12-24 16:42:05 +0800

    add ffmpeg_cut.lua

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # Video Shuffler for Aegisub
 Script for Aegisub to cut video and voice files.   
 在Aegisub中用字幕切割视频和音频文件。
+
+### 用法
+1. 下载[ffmpeg](https://ffmpeg.org/download.html)  
+2. 解压并把ffmpeg添加到系统环境变量中
+3. 下载本仓库文件，复制到aegisub的`automation\autoload`目录中
+4. 打开Aegisub，并加载字幕和视频/音频文件，选择`自动化-对应菜单命令`
+
+![](img/screen_1.png)
+
+## ffmpeg_cut.lua
+使用ffmpeg对选中的字幕所对应的媒体文件进行切割。  
+包含多种模式，根据描述有如下特性：
+
+1. `保存选中每行字幕对应的媒体的为一个`和`保存选中字幕始末范围的媒体为一个`,即输出的结果是一个文件还是多个文件。
+2. `视频`,`视频（keyframe）`和`音频`，即输出视频（速度慢），还是根据关键帧输出视频（时间点可能有问题，输出的音频可能错位，时间过短可能没有画面），还是输出音频。通常输出的格式与源文件的格式相同
+
+## video_shuffler.lua
+使用[video shuffler（洋片箱）](https://github.com/tumuyan/video-shuffler)对字幕和视频进行处理。  
+由于洋片箱是一个Python工具，因此必须额外安装Python环境以及洋片箱这个工具，并且在脚本中设置洋片箱的路径，即脚本中的如下内容：
+```
+video_shuffler = "C:\\prg\\video-shuffler\\main.py"
+```
---
+++ ffmpeg_cut.lua
@@ -0,0 +1,141 @@
+-- Copyright (c) 2022, tumuyan <tumuyan@gmail.com>
+local tr = aegisub.gettext
+
+script_name = tr "ffmpeg cut"
+script_author = "tumuyan"
+script_version = "0.1"
+script_description = "Cut video or audio to clips by ffmpeg. \n用ffmpeg切分选中的字幕对应的视频/音频"
+
+video_suffixs = {"mp4", "mkv", "flv"}
+audio_suffixs = {".mp3", ".wav", ".ogg", ".m4a", ".aac", ".ape", ".flac", ".wma"}
+
+function validate_mult_select(subs, sel)
+    return #sel > 1
+end
+
+-- 检查文件格式是否为视频。
+-- 当文件不是视频时，视频相关的菜单不激活
+function validate_video(subs, sel)
+    suffix = string.lower(string.gsub(aegisub.project_properties().video_file, '.+%.', ''))
+    for k, v in ipairs(video_suffixs) do
+        if v == suffix then
+            return true;
+        end
+    end
+    return false;
+end
+
+-- 检查文件格式是否为音频。
+-- 当文件是视频时，切割音频需要转码
+-- suffix = string.lower(string.gsub(aegisub.project_properties().audio_file, '.+%.', '') )
+function validate_audio(suffix)
+    for k, v in ipairs(audio_suffixs) do
+        if v == suffix then
+            return true;
+        end
+    end
+    return false;
+end
+
+function ms2str(ms)
+    s = ms / 1000.0
+    m = math.floor(s / 60)
+    s = s - 60 * m
+    h = math.floor(m / 60)
+    m = m - 60 * h
+    return "" .. h .. ":" .. m .. ":" .. s
+end
+
+function ffmpeg_cut(subs, sel, to_audio, to_mult, keyframe)
+    script_path = aegisub.decode_path("?script/" .. aegisub.file_name())
+    video_path = aegisub.project_properties().video_file
+    audio_path = aegisub.project_properties().audio_file
+
+    video_suffix = string.lower(string.gsub(video_path, '.+%.', '.'))
+    audio_suffix = string.lower(string.gsub(audio_path, '.+%.', '.'))
+
+    input_path = video_path
+    output_folder = aegisub.decode_path("?video/")
+    output_suffix = video_suffix
+    codec = ""
+    title = tr "Video:"
+
+    if to_audio then
+        input_path = audio_path
+        output_folder = aegisub.decode_path("?audio/")
+        title = tr "Audio:"
+        if validate_audio(audio_suffixs) then
+            output_suffix = audio_suffix
+            codec = " -acodec copy "
+        elseif audio_suffix == ".mp4" then
+            output_suffix = ".m4a"
+            codec = " -vn -acodec copy "
+        else
+            output_suffix = ".wav"
+            codec = " -vn "
+        end
+    elseif keyframe then
+        codec = " -acodec copy -vcodec copy "
+    end 
+
+    local cmd = "ffmpeg -i \"" .. input_path .. "\" -ss "
+
+    if (to_mult) then
+        for _, i in ipairs(sel) do
+            local line = subs[i]
+            local cmd2 = cmd .. ms2str(line.start_time) .. " -to " .. ms2str(line.end_time) .. codec .. " -y \"" ..
+                             output_folder .. line.text .. output_suffix .. "\""
+            -- os.execute("echo " .. cmd2 .. " & pause")
+            aegisub.progress.title(title)
+            aegisub.progress.set(_ * 100 / #sel)
+            aegisub.progress.task(line.text)
+            os.execute(cmd2)
+            -- break
+        end
+    else
+        local line = subs[sel[1]]
+        local start_time = line.start_time
+        local end_time = subs[sel[#sel]].end_time
+        local cmd2 = cmd .. ms2str(start_time) .. " -to " .. ms2str(end_time) .. codec .. " -y \"" .. output_folder ..
+                         line.text .. output_suffix .. "\""
+        os.execute(cmd2 .. " & pause")
+    end
+end
+
+function cut_audio_one(subs, sel)
+    ffmpeg_cut(subs, sel, true, false, false)
+end
+
+function cut_video_one(subs, sel)
+    ffmpeg_cut(subs, sel, false, false, false)
+end
+
+function cut_video_one_keyframe(subs, sel)
+    ffmpeg_cut(subs, sel, false, false, true)
+end
+
+function cut_audio_mult(subs, sel)
+    ffmpeg_cut(subs, sel, true, true, false)
+end
+
+function cut_video_mult(subs, sel)
+    ffmpeg_cut(subs, sel, false, true, false)
+end
+
+function cut_video_mult_keyframe(subs, sel)
+    ffmpeg_cut(subs, sel, false, true, true)
+end
+
+aegisub.register_macro("保存选中每行字幕对应的媒体的为一个" .. tr "Video", script_description,
+    cut_video_mult, validate_video)
+aegisub.register_macro("保存选中每行字幕对应的媒体的为一个" .. tr "Video" .. tr "(Keyframe)",
+    script_description, cut_video_mult_keyframe, validate_video)
+aegisub.register_macro("保存选中每行字幕对应的媒体的为一个" .. tr "Audio", script_description,
+    cut_audio_mult)
+
+aegisub.register_macro("保存选中字幕始末范围的媒体为一个" .. tr "Video", script_description,
+    cut_video_one, validate_video)
+aegisub.register_macro("保存选中字幕始末范围的媒体为一个" .. tr "Video" .. tr "(Keyframe)",
+    script_description, cut_video_one_keyframe, validate_video)
+aegisub.register_macro("保存选中字幕始末范围的媒体为一个" .. tr "Audio", script_description,
+    cut_audio_one)
